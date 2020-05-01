7695074518de18ecf5570f6129ed00c633936e8a
adiojha629 <adiojha629@gmail.com>
2020-05-01 15:09:57 -0500

    Update model_creation_script.py
    added the model that will use for first test

--- model_creation_script.py
+++ model_creation_script.py
@@ -17,7 +17,29 @@ conv1 = tf.keras.layers.Conv2D(32,kernel_size = (3, 3), activation = 'relu', pad
 
 pool1 = tf.keras.layers.MaxPool2D(pool_size = (2,2))(conv1)
 
-out = tf.keras.layers.Dense(2,activation = 'softmax')(pool1)
+conv2 = tf.keras.layers.Conv2D(32,kernel_size = (3, 3), activation = 'relu', padding = 'same')(pool1)
+
+pool2 = tf.keras.layers.MaxPool2D(pool_size = (2,2))(conv2)
+
+flat = tf.keras.layers.Flatten()(pool2)
+
+hidden1 = tf.keras.layers.Dense(256, activation = 'relu')(flat)
+
+dropout1 = tf.keras.layers.Dropout(rate = 0.3)(hidden1)
+
+hidden2 = tf.keras.layers.Dense(256, activation = 'relu')(dropout1)
+
+dropout2 = tf.keras.layers.Dropout(rate = 0.3)(hidden2)
+
+out = tf.keras.layers.Dense(2, activation = 'softmax')(dropout2)
 
 model = tf.keras.Model(inputs = inp, outputs = out)
 
+sgd = tf.keras.optimizers.SGD(lr= .0001)
+adam = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+model.compile(optimizer=adam,
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+# Summarize convolutional neural network architecture
+model.summary()
